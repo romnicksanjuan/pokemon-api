60fd2fe04f69f661e4118d2678ce56344a0e1447
romnick <romnicksanjuan9@gmail.com>
2024-03-15 19:16:26 +0800

    first commit

---
+++ PokemonReactApi/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState, useRef } from 'react'
+import './App.css'
+
+function App() {
+
+  const myFunction = async ({avatar}) => {
+    try {
+      const name = document.getElementById('name').value;
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+      const data = await response.json();
+      console.log(data)
+      const image = document.getElementById('image');
+      const sprite = data.sprites.front_default;
+      image.src = sprite;
+      image.style.display = 'block'
+
+    } catch (error) {
+
+    }
+  }
+  return (
+    <>
+      <div>
+        <input type="text" placeholder='enter pokemon name' id='name' />
+        <button onClick={myFunction}>submit</button>
+        <br />
+        <br />
+
+        <img src="" alt="pokemon" id='image' style={{display:'none'}}/>
+      </div>
+    </>
+  )
+}
+
+export default App
